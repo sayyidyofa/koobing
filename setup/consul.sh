fe11598c2d6e2bb9f084b92f7fae7a8a7a8f6433
Sayyid Yofa <34566219+sayyidyofa@users.noreply.github.com>
2025-05-08 23:45:40 +0700

    trying cleaner approach

--- setup/consul.sh
+++ setup/consul.sh
@@ -17,17 +17,6 @@ sudo rm -rf /etc/machine-id
 sudo systemd-machine-id-setup
 # Set hostname
 sudo hostnamectl set-hostname $HOSTNAME
-sudo tee /etc/hosts > /dev/null <<EOF
-127.0.0.1 localhost
-127.0.1.1 $HOSTNAME
-
-# The following lines are desirable for IPv6 capable hosts
-::1     ip6-localhost ip6-loopback
-fe00::0 ip6-localnet
-ff00::0 ip6-mcastprefix
-ff02::1 ip6-allnodes
-ff02::2 ip6-allrouters
-EOF
 sudo sed -i 's/#DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
 sudo systemctl enable --now systemd-resolved
 sudo systemctl restart systemd-resolved
