1a5c506f5f05b76620d68e470cabd313ea93b935
Sayyid Yofa <34566219+sayyidyofa@users.noreply.github.com>
2025-05-15 19:45:23 +0700

    etcd ssl

--- setup/etcd.sh
+++ setup/etcd.sh
@@ -5,6 +5,9 @@ set -eu
 HOSTNAME=$1
 ROOT_DOMAIN=$2
 VERSION=$3
+CERT_PATH=$4
+CERT_PK_PATH=$5
+CA_PATH=$6
 
 wget "https://github.com/etcd-io/etcd/releases/download/$VERSION/etcd-$VERSION-linux-amd64.tar.gz"
 tar -zxvf etcd-$VERSION-linux-amd64.tar.gz
@@ -20,16 +23,47 @@ sudo mkdir -p /var/lib/etcd
 sudo rm -rf /etc/etcd
 sudo mkdir -p /etc/etcd
 sudo tee /etc/etcd/conf.yml > /dev/null <<EOF
-name $HOSTNAME 
-discovery-srv service.$ROOT_DOMAIN 
-initial-advertise-peer-urls https://$HOSTNAME.node.homelab.$ROOT_DOMAIN:2380 
-initial-cluster-token etcd-homelab 
-initial-cluster-state new 
-advertise-client-urls https://$HOSTNAME.node.homelab.$ROOT_DOMAIN:2379 
-listen-client-urls https://0.0.0.0:2379 
-listen-peer-urls https://0.0.0.0:2380 
-data-dir /var/lib/etcd
-name: 'default'
+name: $HOSTNAME 
+discovery-srv: service.$ROOT_DOMAIN 
+initial-advertise-peer-urls: https://$HOSTNAME.node.homelab.$ROOT_DOMAIN:2380 
+initial-cluster-token: etcd-homelab 
+initial-cluster-state: new 
+advertise-client-urls: https://$HOSTNAME.node.homelab.$ROOT_DOMAIN:2379 
+listen-client-urls: https://0.0.0.0:2379 
+listen-peer-urls: https://0.0.0.0:2380 
+data-dir: /var/lib/etcd
+client-transport-security:
+  # Path to the client server TLS cert file.
+  cert-file: $CERT_PATH
+
+  # Path to the client server TLS key file.
+  key-file: $CERT_PK_PATH
+
+  # Enable client cert authentication.
+  client-cert-auth: false
+
+  # Path to the client server TLS trusted CA cert file.
+  trusted-ca-file: $CA_PATH
+
+  # Client TLS using generated certificates
+  auto-tls: false
+
+peer-transport-security:
+  # Path to the peer server TLS cert file.
+  cert-file: $CERT_PATH
+
+  # Path to the peer server TLS key file.
+  key-file: $CERT_PK_PATH
+
+  # Enable peer client cert authentication.
+  client-cert-auth: false
+
+  # Path to the peer server TLS trusted CA cert file.
+  trusted-ca-file: $CA_PATH
+
+  # Peer TLS using generated certificates.
+  auto-tls: false
+
 EOF
 
 sudo tee /etc/systemd/system/etcd.service > /dev/null <<EOF
